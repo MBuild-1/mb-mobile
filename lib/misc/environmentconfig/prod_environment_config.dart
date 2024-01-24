969dc2897a89bad256490141f91c4b2bed20dd58
Nanda Prasetio <nandaprasesoft98@gmail.com>
2024-01-24 13:28:41 +0700

    feat: revert prod base url

--- lib/misc/environmentconfig/prod_environment_config.dart
+++ lib/misc/environmentconfig/prod_environment_config.dart
@@ -4,7 +4,7 @@ class ProdEnvironmentConfig extends EnvironmentConfig {
   @override
   String get environment => "prod";
   @override
-  String get baseUrl => "https://8cf6-2404-8000-1004-91e2-75b6-a98d-e05c-6f07.ngrok-free.app/v1/mobile/";
+  String get baseUrl => "https://api.masterbagasi.com/v1/mobile/";
   @override
   String get elasticEntryEndpointPath => "entry";
   @override
