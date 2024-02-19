847a1a784474cf84bc2334e0ee0918d8838945eb
Nanda Prasetio <nandaprasesoft98@gmail.com>
2024-02-19 15:57:43 +0700

    fix: add copy tracking number in order tracking item widget

--- lib/presentation/widget/order/order_tracking_item.dart
+++ lib/presentation/widget/order/order_tracking_item.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:masterbagasi/misc/ext/number_ext.dart';
 import 'package:masterbagasi/misc/ext/string_ext.dart';
 
 import '../../../domain/entity/order/ordertracking/order_tracking.dart';
 import '../../../misc/constant.dart';
+import '../../../misc/toast_helper.dart';
 import '../colorful_chip.dart';
+import '../tap_area.dart';
 
 class OrderTrackingItem extends StatelessWidget {
   final OrderTracking orderTracking;
@@ -33,23 +36,55 @@ class OrderTrackingItem extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            orderTracking.trackingNumber.toStringNonNull,
-                            style: TextStyle(
-                              color: Theme.of(context).colorScheme.primary,
-                              fontWeight: FontWeight.bold
-                            ),
-                          ),
-                          Text(
-                            "${"Weight".tr}: ${orderTracking.weight.toWeightStringDecimalPlaced()} Kg",
-                            style: const TextStyle(
-                              fontSize: 13.0
-                            ),
-                          ),
-                        ],
+                      child: Builder(
+                        builder: (context) {
+                          String trackingNumber = orderTracking.trackingNumber.toStringNonNull;
+                          return Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    trackingNumber,
+                                    style: TextStyle(
+                                      color: Theme.of(context).colorScheme.primary,
+                                      fontWeight: FontWeight.bold
+                                    ),
+                                  ),
+                                  Text(
+                                    "${"Weight".tr}: ${orderTracking.weight.toWeightStringDecimalPlaced()} Kg",
+                                    style: const TextStyle(
+                                      fontSize: 13.0
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(width: 8.0),
+                              TapArea(
+                                onTap: () {
+                                  dynamic rawClipboardData = trackingNumber;
+                                  ClipboardData? clipboardData;
+                                  if (rawClipboardData is String) {
+                                    clipboardData = ClipboardData(text: rawClipboardData);
+                                  } else if (rawClipboardData is num) {
+                                    clipboardData = ClipboardData(text: rawClipboardData.toString());
+                                  }
+                                  if (clipboardData != null) {
+                                    Clipboard.setData(clipboardData);
+                                    ToastHelper.showToast("${"Success copied".tr}.");
+                                  } else {
+                                    ToastHelper.showToast("${"Cannot copy this content".tr}.");
+                                  }
+                                },
+                                child: const Icon(
+                                  Icons.copy,
+                                  size: 18
+                                )
+                              ),
+                            ],
+                          );
+                        }
                       ),
                     ),
                     ColorfulChip(
